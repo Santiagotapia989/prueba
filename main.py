cea243d90301828d3cefc6477d1db3acb75c0868
Santiagotapia989 <sbazan@fie.undef.edu.ar>
2026-04-27 16:09:50 -0300

    agrego endpoints saludo y config

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
+# Endpoint principal
+@app.get("/")
+def home():
+    return {"mensaje": "API funcionando 🚀"}
+
+# Nuevo endpoint
 @app.get("/saludo")
 def saludo():
     return {"mensaje": "Hola desde Railway 🚀"}
+
+# Endpoint con variable de entorno
+@app.get("/config")
+def config():
+    return {"app": os.getenv("APP_NAME")}
